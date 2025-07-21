af9544f747446f3152cbaf55f0f7ea23ba37f4a5
Hayat Ahmad <sayed.hayat05@hotmail.com>
2025-07-21 12:10:04 +0500

    Fixed Issue

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def add_numbers(x,y,z):
- return x+y+z
+def add_numbers(x, y):
+    return x+y
 
 
-def sort_list(unsorted_list:List):
- List.sort()
+def sort_list(unsorted_list: list):
+    return sorted(unsorted_list) # there was no return plus wrong function
 
 
-if __name__ == __main__:
- print(add_numbers(2,3,4))
- print(sort_list([2,3,5.2,1])
+if __name__ == "__main__":
+    print(add_numbers(2, 3))
+    print(sort_list([2, 3, 5.2, 1]))
+    #needed indent plus no quote marks
 
  
