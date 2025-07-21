411c90a27530978c71d1856a7ddd9bfacbc3d11a
Saad Saleem <saad.xsaleem@gmail.com>
2025-07-21 12:08:25 +0500

    adding multiple numbers and bugs fix

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-def add_numbers(x,y):
- return x+y
+from typing import List
+
+def add_numbers(num_list:List):
+    sum = 0
+    for num in num_list:
+      sum = sum + num
+    return sum
 
 
 def sort_list(unsorted_list:List):
- List.sort()
+ List.sort(unsorted_list)
+ return unsorted_list
 
 
-if __name__ == __main__:
- print(add_numbers(2,3))
- print(sort_list([2,3,5.2,1])
+if __name__ == "__main__":
+    num_list = [2,3,5.2,1]
+    print(add_numbers(num_list))
+    print(sort_list(num_list))
 
  
