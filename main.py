1da234390764f2ad8829467d7e93fab2d1722d39
AHadi430 <lahore.284@gmail.com>
2025-07-21 12:36:43 +0500

    ggs

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-def add_numbers(x,y):
- return x+y
+from typing import List
 
+def add_numbers(numbers: List[float]):
+    return sum(numbers)
 
-def sort_list(unsorted_list:List):
- List.sort()
+def sort_list(unsorted_list: List[float]):
+    return sorted(unsorted_list)
 
+if __name__ == "__main__":
 
-if __name__ == __main__:
- print(add_numbers(2,3))
- print(sort_list([2,3,5.2,1])
-
- 
+    print(add_numbers([2, 3, 5, 10]))         
+    print(sort_list([2, 3, 5.2, 1]))          
